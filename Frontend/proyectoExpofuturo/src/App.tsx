e1c62b62a435bc1fd78b1a3e682dc15f15f6346d
sara-34 <saritaariaslondono12@gmail.com>
2024-05-18 09:29:52 -0500

    subiendo login

--- Frontend/proyectoExpofuturo/src/App.tsx
+++ Frontend/proyectoExpofuturo/src/App.tsx
@@ -8,26 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <h2>Login</h2>
+      <section className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centerd is-primary">
+              <div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
